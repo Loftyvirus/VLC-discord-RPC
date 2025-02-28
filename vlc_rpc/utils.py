977ae57ee516408579d948a004466a70b7dc8f24
ryuzinoh <ryuzinoh@users.noreply.github.com>
2025-03-01 03:48:36 +0545

    UPDATE: enhance load_config to specify .env path and log CLIENT_ID

--- vlc_rpc/utils.py
+++ vlc_rpc/utils.py
@@ -2,5 +2,9 @@ import os
 from dotenv import load_dotenv
 
 def load_config():
-    load_dotenv()
-    return os.getenv("CLIENT_ID")
+    env_path = os.path.join(os.path.dirname(__file__), '..', 'config', '.env')
+    load_dotenv(dotenv_path=env_path)
+
+    client_id = os.getenv("CLIENT_ID")
+    print(f"Loaded CLIENT_ID: {client_id}")
+    return client_id
